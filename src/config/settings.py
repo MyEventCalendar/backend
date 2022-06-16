20b436d934fc57271a1840b527ff6e473683ae5e
spiranscadaver <fatum_oksi@mail.ru>
2022-06-16 15:07:38 +0300

    delete blank line

--- src/config/settings.py
+++ src/config/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'health_check',                             # required
     'health_check.db',                          # stock Django health checkers
 
-
 ]
 
 MIDDLEWARE = [
